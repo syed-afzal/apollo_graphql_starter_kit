8eda1034ca370c8667b913f043d464d1deebaa4d
Afzal <sah.afzal@gmail.com>
2020-11-11 18:23:11 +0500

    inntial commit :tada:

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors');
+const dotEnv = require('dotenv');
+
+// set env variables
+dotEnv.config();
+
+const app = express();
+
+// cors
+app.use(cors());
+
+// body parser middleware
+app.use(express.json());
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    greetings: String
+  }
+`;
+
+const resolvers = {};
+
+const apolloServer = new ApolloServer({
+   typeDefs,
+   resolvers
+});
+
+apolloServer.applyMiddleware({app, path:'/graphql'});
+
+const PORT = process.env.PORT | 3000;
+
+app.use('/', (req, res, next) => {
+    res.send('Hello');
+});
+
+app.listen(PORT, () => {
+   console.log(`Server is up on PORT ${PORT}`);
+   console.log(`GraphQl endpoint ${apolloServer.graphqlPath}`);
+});
