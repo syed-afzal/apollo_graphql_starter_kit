4c84008cc03393e44868b8adfd28fbfa60a94672
Afzal <sah.afzal@gmail.com>
2020-12-05 20:57:20 +0500

    Bug resolved :bug:

--- resolvers/middleware/index.js
+++ resolvers/middleware/index.js
@@ -17,7 +17,7 @@ module.exports.isTaskOwner = async (_, { id }, { loggedInUserId }) => {
         const task = await Task.findById(id);
         if (!task) {
             throw new Error("Task not found")
-        } else if (task.user.toString() !== loggedInUserId ) {
+        } else if (task.user.toString() != loggedInUserId ) {
             throw new Error("Not authorized as task owner");
         }
         return skip;
